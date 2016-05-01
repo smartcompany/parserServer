a44f70251af223f19cd93464696f00b8fca1dff1
Kim Yong Geon <Smart@Kimui-MacBook-Pro.local>
2016-05-01 21:31:09 +0900

    test

---
+++ index.js
@@ -0,0 +1,51 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI || 'mongodb://smart:k12345@ds019078.mlab.com:19078/smart';
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://smart:k12345@ds019078.mlab.com:19078/smart',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || '6oNn8UQpvU8r4Jl3ABAvxLqlIQHlpiAiPr2gNvQZ',
+  masterKey: process.env.MASTER_KEY || 'Sio1RdFtsXxa9Qm29kE7pNe0e4j92wZuEYANcTt4', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://localhost:1337',  // Don't forget to change to https if needed
+  facebookAppIds : process.env.FACEBOOK_APP_ID || '913764205386593',
+  restAPIKey : 'rbaNGUJJh9xAUfikGKJNoSik3MGhxaIcQqcXYHYz',
+  clientKey : 'xEGyDaoAZEgFm7UNEzebx8YucSmHRvue1NoaTYNG',
+  dotNetKey : 'I93cw1kNOgKfi1GFWFyNvckTq0dwsUrgyLX60IsI',
+  javascriptKey : 'oW1NGaKxHYeZCzLpfB5oRgh16POR7RehfsnK4ipV',
+  fileKey : '3daed3f9-02bf-4a47-a431-c9c4bda78ad2',
+  oauth:
+  {
+    facebook:
+    {
+        appIds: "913764205386593"
+    }
+  }
+});
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
+var app = express();
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a web site.');
+});
+
+var port = process.env.PORT || 1337;
+app.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
